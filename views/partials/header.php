f49f7ea558459571ee312ef275055288cb3791db
Ma Angelica A Arevalo <76656116+MAAArevalo@users.noreply.github.com>
2025-08-11 01:47:46 +0800

    fix responsive

--- views/partials/header.php
+++ views/partials/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>What's the Weather</title>
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lipis/flag-icons@7.3.2/css/flag-icons.min.css" />
