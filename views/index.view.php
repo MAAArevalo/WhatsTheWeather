35c369bc9f300b89a877ee1768e268986fc45e3f
Ma Angelica A Arevalo <76656116+MAAArevalo@users.noreply.github.com>
2025-08-19 12:37:36 +0800

    Update index.view.php

--- views/index.view.php
+++ views/index.view.php
@@ -16,18 +16,21 @@ get_header();
                 <input type="text" value="" placeholder="Search Country or Cities" class="w-full p-3 border-b border-b-gray-800">
             </form>
         </div>
-        <div class="city-blocks flex gap-5 flex-wrap justify-center">
-            <?php 
-                foreach ($countries as $country): 
-                    $countryname = $country['name'];
-                    $countrycode = strtolower( $country['code'] );
-            ?>
-            <div data-value="<?php echo $countrycode; ?>" class="city p-3 w-1/3 lg:w-1/6 md:w-1/4 text-center border border-sky-100 h-[100px] cursor-pointer flex flex-col justify-center items-center hover:bg-sky-50">
-                <div class="flag"><span class="fi fi-<?php echo $countrycode; ?>"></span></div>
-                <?php echo $countryname; ?>
+        <div id="content">
+            <div class="city-blocks flex gap-5 flex-wrap justify-center">
+                <?php 
+                    foreach ($countries as $country): 
+                        $countryname = $country['name'];
+                        $countrycode = strtolower( $country['code'] );
+                ?>
+                <div data-value="<?php echo $countrycode; ?>" class="city p-3 w-1/3 lg:w-1/6 md:w-1/4 text-center border border-sky-100 h-[100px] cursor-pointer flex flex-col justify-center items-center hover:bg-sky-50">
+                    <div class="flag"><span class="fi fi-<?php echo $countrycode; ?>"></span></div>
+                    <?php echo $countryname; ?>
+                </div>
+                <?php endforeach; ?>
             </div>
-            <?php endforeach; ?>
         </div>
+
     </div>
 </section>
 <?php get_footer(); ?>
