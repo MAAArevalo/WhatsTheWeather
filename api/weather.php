6cb4c23aaa2a1d781108551f34acaf61594e1986
Ma Angelica A Arevalo <76656116+MAAArevalo@users.noreply.github.com>
2025-08-19 12:33:21 +0800

    Updating search and few code fixes

--- api/weather.php
+++ api/weather.php
@@ -1,34 +1,41 @@
 <?php
 
-/* Open weather city list */
-function getCity( ){
-    $city_data = file_get_contents( 'data/city.list.json' );
-    return json_decode( $city_data, true );
+class weather {
+    public function __construct() {
+        //$this->getWeather();
+    }
+
+    /* Open weather city list */
+    static function getCity( ){
+        $city_data = file_get_contents( 'data/city.list.json' );
+        return json_decode( $city_data, true );
+    }
+
+    static function getCountry( ){
+        $country_data = file_get_contents( 'data/countries.json' );
+        return json_decode( $country_data, true );
+    }
+
+    /* Free API from openweathermap.org */
+
+    function getWeather( $params = '' ) {
+
+        $city = $params;
+        $key = "088d99bf3f0e3d3be6010cf442a33a16";
+        $url = "https://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$key}";
+
+
+
+        $ch = curl_init();
+        curl_setopt( $ch, CURLOPT_URL, $url );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+
+        $response = curl_exec( $ch );
+
+        curl_close( $ch );
+
+        return json_decode( $response, true );
+
+    }
 }
 
-function getCountry( ){
-    $country_data = file_get_contents( 'data/countries.json' );
-    return json_decode( $country_data, true );
-}
-
-/* Free API from openweathermap.org */
-
-function getWeather( $params = '' ) {
-
-    $city = $params;
-    $key = "088d99bf3f0e3d3be6010cf442a33a16";
-    $url = "https://api.openweathermap.org/data/2.5/weather?q={$city}&appid={$key}";
-
-
-
-    $ch = curl_init();
-    curl_setopt( $ch, CURLOPT_URL, $url );
-    curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-
-    $response = curl_exec( $ch );
-
-    curl_close( $ch );
-
-    return json_decode( $response, true );
-
-}
--- controllers/index.php
+++ controllers/index.php
@@ -1,13 +1,40 @@
 <?php
 
+$search = "";
+if(isset($_GET['s'])) $search = $_GET['s'];
+
 require_once 'api/weather.php';
 
-$weather = getWeather();
-$city = getCity();
+
+$city = weather::getCity();
+$countries = weather::getCountry();
+$country_col = array_map('mb_strtolower', array_column($countries, 'name'));
+
+class index{
+    
+    public function __construct() {
+
+    }
+
+    function search($s, $country){
+        if(empty($s)){ return; }
+
+        if(in_array($s, $country, true)){
+            return "found";
+        }else{
+            return "not found";
+        }
+    }
+}
+
+$main = new index();
+
+
+$s_val = $main->search($search, $country_col);
+// echo '<pre>';
+// print_r($countries);
+// echo '</pre>';
+print_r($s_val);
+
 
 require 'views/index.view.php';
-
-
-
-
-
